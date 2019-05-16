ed246946bc8b455d64cb64b972649b0a171ef6fd
Lucas Pompa <lucas1.pompa@supinternet.fr>
2019-05-16 17:26:18 +0200

    first commit

--- src/Controller/CesarController.php
+++ src/Controller/CesarController.php
@@ -1,16 +1,18 @@
 <?php
 namespace Controller;
 use ThirtySix\Connexion;
-
+use Model\Nominee;
+use Model\User;
 class CesarController{
   public function gagnantsAction(){
-    $winners = Nominee::getWinners($pdo);
+    
+    $winners = \Model\Nominee::getWinners($pdo);
 
-    $bestplayers = User::getBest($pdo);
+    $bestplayers = \Model\User::getBest($pdo);
 
     $categories = [];
     foreach ($winners as $winner) {
-      $categories[$winner['category_id']] = Category::getById($pdo, $winner['category_id']);
+      $categories[$winner['category_id']] = \Model\Category::getById($pdo, $winner['category_id']);
     }
     include "./winners.php";
   }
